df8bd9ba34021c65d71c6497e7329f42b84c5b1a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-03-25 21:33:19 +0000

    simple component initial stage

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   "stories": [
-   "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+   //"../stories/**/*.stories.mdx",
+   //"../stories/**/*.stories.@(js|jsx|ts|tsx)"
+   '../app/components/**/*.stories.js'
   ],
   "addons": [
     "@storybook/addon-links",
